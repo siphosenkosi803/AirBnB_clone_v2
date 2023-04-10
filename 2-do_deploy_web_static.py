af9525bc8ef049bd186b8f791b52e62a56203006
siphosenkosi803 <110998052+siphosenkosi803@users.noreply.github.com>
2023-04-10 14:01:08 +0200

    Update 2-do_deploy_web_static.py

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,31 +1,49 @@
 #!/usr/bin/python3
-"""Fabric script that distributes an archive to web servers"""
-from fabric.api import env, put, run
+""" Fabric module """
+from fabric.api import env
+from fabric.api import local
+from fabric.api import run
+from fabric.api import put
+from datetime import datetime
 import os
-
 env.hosts = ['52.87.221.0', '18.209.152.146']
+env.usre = 'ubuntu'
+
+
+def do_pack():
+    """Archive the content of web_static folder"""
+    time = datetime.utcnow().strftime('%Y%m%d%H%M%S')
+    file_path = "versions/web_static_{}.tgz".format(time)
+    cmd = "tar -cvzf {} web_static/*".format(file_path)
+    local("mkdir -p versions")
+    local(cmd)
+    if os.path.exists(file_path):
+        return file_path
+    else:
+        return None
+
 
 def do_deploy(archive_path):
-    """Distributes an archive to web servers"""
+    """Distribute an Archive to servers"""
     if not os.path.exists(archive_path):
-        return False
-
+        return false
     try:
-        file_name = os.path.basename(archive_path)
-        file_name_no_ext = os.path.splitext(file_name)[0]
-        remote_path = "/tmp/{}".format(file_name)
-        data_path = "/data/web_static/releases/{}".format(file_name_no_ext)
+        file_name = archive_path.split("/")[-1].split(".")[0]
 
-        put(archive_path, remote_path)
-        run("mkdir -p {}".format(data_path))
-        run("tar -xzf {} -C {}".format(remote_path, data_path))
-        run("rm {}".format(remote_path))
-        run("mv {}/web_static/* {}".format(data_path, data_path))
-        run("rm -rf {}/web_static".format(data_path))
-        run("rm -rf /data/web_static/current")
-        run("ln -s {} /data/web_static/current".format(data_path))
-
+        put(archive_path, '/tmp/')
+        # ! just for testing
+        # //run('rm -rf /data/web_static/releases/*')
+        run('mkdir /data/web_static/releases/{}'.format(file_name))
+        run('tar -xzf /tmp/{}.tgz -C /data/web_static/releases/{}'.
+            format(file_name, file_name))
+        run('mv /data/web_static/releases/{}/web_static/* \
+            /data/web_static/releases/{}/'.
+            format(file_name, file_name))
+        run('rm -rf /data/web_static/releases/{}/web_static'.format(file_name))
+        run('rm -rf /tmp/{}.tgz'.format(file_name))
+        run('rm -rf /data/web_static/current')
+        run('ln -sf /data/web_static/releases/{}/ /data/web_static/current'.
+            format(file_name))
         return True
     except:
         return False
-    
