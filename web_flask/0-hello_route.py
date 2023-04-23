25edb791088748084f7f13a3206c07f2f3964fa9
siphosenkosi803 <sibusisoradebe943@yahoo.com>
2023-04-23 10:09:56 +0000

    commit 0-hello_route.py

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""WebFlask"""
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_nflask():
+    """Dispaly the stipulated text"""
+    return 'Hello HBNB!'
+
+
+def start_app():
+    try:
+        app.run(host='0.0.0.0', port=5000)
+    except Exception as e:
+        print(f"There was an error initializing flask {e}")
+
+
+if __name__ == '__main__':
+    start_app()
+
